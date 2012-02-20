88a587b03ecc9db9ee5af8494a157aba919b4a8c
sl0.self <sl0.self@googlemail.com>
2012-02-20 20:22:03 +0100

    findcnt.py corrected, due to errors
    Option -d now takes a single file or a directory, which is recursivly searched
    Option for file of files removed, wasn't neccessary

--- findcnt.py
+++ findcnt.py
@@ -27,9 +27,35 @@ Name2    ;Treffer21;Treffer22;   ;Treffer2N;2Nges
 from UserDict import UserDict
 from UserList import UserList
 from optparse import OptionParser
+from stat import ST_MODE, S_ISDIR, S_ISREG
 import re
 import sys
 import os
+
+def count_one_pattern_one_line(muster, zeile):
+    """ein Muster in einer Zeile Text finden"""
+    retval = 0
+    mat = re.search(muster, zeile)
+    if mat:
+        retval = 1
+    return retval
+
+
+def count_one_pattern_one_file(muster, datei):
+    """ein Muster in einer Datei finden"""
+    counter = 0
+    try:
+        fil0 = open(datei, 'r')
+        while True:
+            line = fil0.readline()
+            if not line:
+                break
+            counter += count_one_pattern_one_line(muster, line)
+        fil0.close()
+    except IOError, err:
+        print datei + ": ", err.strerror
+    return counter
+
 
 class Patterns(UserDict):
     """Patterns object contains content of a file, one
@@ -49,9 +75,7 @@ class Patterns(UserDict):
         self.data[1] = pattern
 
     def __read_pattern_file(self, filename):
-        """reads file and creates self.data
-        for every line read successfully
-        """
+        """reads file and creates self.data for every line"""
         try:
             pfile = open(filename, 'r')
             for zeile in pfile:
@@ -59,7 +83,8 @@ class Patterns(UserDict):
                 self.data[self.cnt] = line
                 self.cnt += 1
         except IOError, err:
-            print filename + ": ", err.strerror
+            print "No pattern file: " + filename + ": ", err.strerror
+            sys.exit(1)
 
     def __repr__(self):
         """representaion of pattern-Object for printouts"""
@@ -78,49 +103,54 @@ class Patterns(UserDict):
     def headline(self):
         """do a nice Startline for our csv"""
         lin = ""
-        #lin += u'"Dateiname\Muster";'
         lin += u'Dateiname\Muster;'
         for mus in self.data.values():
             #such = "\"%s\"" % (mus)
             such = "%s" % (mus)
             lin += such
             lin += ';'
-        #lin += u'"Summe"'
         lin += u'Summe'
         print str(lin)
 
-class findcnt(UserList):
-    """findcnt object contains content of a file, one filename each line
-    """
 
-    def __init__(self, filename=None):
-        """provide the list object"""
+class findcnt(UserList):
+    """targets object contains a list of files"""
+    
+    def __init__(self, filename=None, pattern=None):
+        """provide the list object as a list of files"""
         UserList.__init__(self)
         self.data = []
-        self.filetype = ""
+        if pattern == None:
+            self.filetype = "[A-Za-z0-9]"
+        else:
+            self.filetype = pattern
         self.path = "."
-        if filename:
-            self.__read_file(filename)
+        try:
+            mode = os.stat(filename)[ST_MODE]
+            if S_ISDIR(mode):
+                self.read_the_filenames(filename)
+            elif S_ISREG(mode):
+                self.data.append(filename)
+            else:
+                print 'Skipping %s' % filename
+        except OSError, err:
+            print "Error: " + err.strerror + ": " +filename
+            sys.exit(1)
 
-    def set_filetype(self, ftype):
-        """estension means filename ending"""
-        self.filetype = ftype
-
-    def read_many_files(self, pathname):
-        """Returns all the filenamess in a directory as a list"""
+    def read_the_filenames(self, pathname):
+        """Returns all filenamess in a given directory as a list"""
         self.path = pathname
         try:
             for dirpath, dirnames, filenames in \
                     os.walk(pathname, topdown=False):
-                #pylint meckert wg. unused dirnames 
+                #pylint mecket wg. unused dirnames
                 for fil in filenames:
                     full = os.path.join(dirpath, fil)
-                    if len(self.filetype):
-                        if re.search(self.filetype, fil):
-                            self.data.append(full)
+                    if re.search(self.filetype, fil):
+                        #if not fil in self.data:
+                        self.data.append(full)
         except UnboundLocalError:
             pass
-
 
     def __read_file(self, filename):
         """initial read file into object"""
@@ -148,29 +178,6 @@ class findcnt(UserList):
             #    continue
             retstr += st0
         return retstr
-
-def count_one_pattern_one_line(muster, zeile):
-    """ein Muster in einer Zeile Text finden"""
-    retval = 0
-    mat = re.search(muster, zeile)
-    if mat:
-        retval = 1
-    return retval
-
-def count_one_pattern_one_file(muster, datei):
-    """ein Muster in einer Datei finden"""
-    counter = 0
-    try:
-        fil0 = open(datei, 'r')
-        while True:
-            line = fil0.readline()
-            if not line:
-                break
-            counter += count_one_pattern_one_line(muster, line)
-        fil0.close()
-    except IOError, err:
-        print datei + ": ", err.strerror
-    return counter
 
 
 class MyCounters(object):
@@ -221,47 +228,36 @@ class MyCounters(object):
 def main():
     """ main(), for test and daily usage
     Options, 2 must be given
-    -d für den Pfad (zu den Dateien, soll rekursiv durchsucht werden)
+    -d für Datei oder Pfad (zu den Dateien, soll rekursiv durchsucht werden)
     -t für den Dateityp (wenn nicht angegeben, alle Dateien durchsuchen)
-    -f für eine Datei mit Dateinamen (Alternative zu -d)
     -l Suchwort-Liste
     -s einzelnes Suchwort
     """
     usage = "usage: %prog --help | -h \n\n%prog version 0.9"
     parser = OptionParser(usage)
     parser.disable_interspersed_args()
-    # long options not used for better readability
+    # long options not used for oldfashioned unix guys better readability
     parser.add_option("-s", "", #"--pattern",
                       dest = "pattern",
-                      help="single pattern, may be regex, OR:")
+                      help = "single pattern, may be regex, OR:")
     parser.add_option("-l", "", #"--pattern-list",
                       dest = "pattern_file",
-                      help="file containing patterns, one per line")
-    parser.add_option("-f", "", #"--file-of-files",
+                      help ="file containing patterns, one per line")
+    parser.add_option("-d", "", #"--path-to-file(s)",
                       dest = "sources_file",
-                      help="use this file only OR:")
-    parser.add_option("-d", "", #"--path-to-files",
-                      dest = "path_to_files",
-                      help="directory, searched for plain files (recursive)")
+                      help = "single file OR: " +
+                            "search recursive in this directory")
     parser.add_option("-t", "", #"--file-type",
                       dest = "sources_type",
-                      help="resrict filenames to file-type, f.e.: *tex")
+                      help = "resrict filenames to file-type, f.e.: .php$")
     (options, args) = parser.parse_args()
     hlp = "\n\tplease use \"--help\" as argument, abort!\n"
-    if options.sources_type == None:
-        filetype = "[A-Za-z0-9]"
-    else:
-        filetype = options.sources_type
-    if options.sources_file == None and options.path_to_files == None:
-        print "\tNo files or directory given, " + hlp
+    if options.sources_file == None:
+        print "\tNo files or directory to be searched given, " + hlp
         sys.exit(1)
     else:
-        if options.sources_file:
-            srcs = findcnt(options.sources_file)
-        else:
-            srcs = findcnt()
-            srcs.set_filetype(filetype)
-            srcs.read_many_files(options.path_to_files)
+        """we have a file or path now"""
+        srcs = findcnt(options.sources_file, options.sources_type)
     if options.pattern_file == None and options.pattern == None:
         print "\tNo patterns_file given, " + hlp
         sys.exit(1)
